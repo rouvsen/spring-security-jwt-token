470e3874b1022ff9a456b76399bcd97380444764
rouvsen <rouvsen@gmail.com>
2024-03-21 22:32:21 +0400

    refactor AuthenticationConfiguration

--- src/main/java/com/rouvsen/springsecutiryjwttoken/security/SecurityConfig.java
+++ src/main/java/com/rouvsen/springsecutiryjwttoken/security/SecurityConfig.java
@@ -26,21 +26,19 @@ public class SecurityConfig {
     private final JwtAuthFilter authFilter;
     private final UserService userService;
     private final PasswordEncoder passwordEncoder;
-    private final AuthenticationConfiguration authenticationConfiguration;
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(x ->
-                        x.requestMatchers("/auth/addUser/**", "/auth/generateToken/**").permitAll()
+                        x.requestMatchers("/auth/welcome/**", "/auth/addUser/**", "/auth/generateToken/**").permitAll()
                         .requestMatchers("/auth/user/**").hasRole("USER")
                         .requestMatchers("/auth/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(x -> x.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider())
-                .authenticationManager(authenticationManager())
                 .addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
@@ -54,7 +52,7 @@ public class SecurityConfig {
     }
 
     @Bean
-    public AuthenticationManager authenticationManager() throws Exception {
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
     }
 }
