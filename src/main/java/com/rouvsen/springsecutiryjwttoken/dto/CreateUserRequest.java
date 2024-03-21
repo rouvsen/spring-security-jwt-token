73a116a38b639f2a2d4ac5bd831e3166b7db91a5
rouvsen <rouvsen@gmail.com>
2024-03-21 22:31:57 +0400

    Refactor grant user func with default authority

--- src/main/java/com/rouvsen/springsecutiryjwttoken/dto/CreateUserRequest.java
+++ src/main/java/com/rouvsen/springsecutiryjwttoken/dto/CreateUserRequest.java
@@ -1,16 +1,11 @@
 package com.rouvsen.springsecutiryjwttoken.dto;
 
-import com.rouvsen.springsecutiryjwttoken.model.Role;
 import lombok.Builder;
-
-import java.util.Set;
-
 
 @Builder
 public record CreateUserRequest(
         String name,
         String username,
-        String password,
-        Set<Role> authorities
+        String password
 ) {
 }
