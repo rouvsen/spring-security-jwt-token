a704f8b58d63f5397ca8be0e8bd6748ef4aaf6b4
rouvsen <rouvsen@gmail.com>
2024-03-21 20:57:46 +0400

    Add JwtService for creating and validation tokens

---
+++ src/main/java/com/rouvsen/springsecutiryjwttoken/service/JwtService.java
@@ -0,0 +1,67 @@
+package com.rouvsen.springsecutiryjwttoken.service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class JwtService {
+
+    @Value("${jwt.key}")
+    private String SECRET;
+
+    public String generateToken(String username) {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("githubName", "rouvsen");
+        return createToken(claims, username);
+    }
+
+    public boolean validateToken(String token, UserDetails userDetails) {
+        String username = extractUsername(token);
+        Date expiration = extractExpiration(token);
+        return userDetails.getUsername().equals(username) && expiration.after(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(getSignKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+        return claims.getExpiration();
+    }
+
+    public String extractUsername(String token) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(getSignKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+        return claims.getSubject();
+    }
+
+    private String createToken(Map<String, Object> claims, String username) {
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(username)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24))
+                .signWith(getSignKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    private Key getSignKey() {
+        byte[] decoded = Decoders.BASE64.decode(SECRET);
+        return Keys.hmacShaKeyFor(decoded);
+    }
+}
